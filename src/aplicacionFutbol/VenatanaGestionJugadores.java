5124298ae7fa76fac2101d118febb679a55938cc
claraurq <claraurq@gmail.com>
2025-01-27 19:02:36 +0100

    GestionJugadores Con Imagenes
    Se añaden la ruta de la imagen del jugador como atributo del objeto jugador.

--- src/aplicacionFutbol/VenatanaGestionJugadores.java
+++ src/aplicacionFutbol/VenatanaGestionJugadores.java
@@ -14,7 +14,9 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
@@ -22,22 +24,28 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class VenatanaGestionJugadores extends JFrame implements ActionListener, WindowListener{
 
 	private static final long serialVersionUID = 1L;
+	private JPanel body;
 	private JPanel contentPane;
 	private JButton btnAñadir;
 	private JButton btnEliminar;
 	private JButton btnModificar;
+	private ImageIcon fotoIcon;
 	private JList<Jugador> lstJugadores;
 	private static DefaultListModel<Jugador> dlm;
 	private boolean modificado = false;
@@ -45,9 +53,12 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 	private boolean modificando = false;
 	private static String nombre;
 	private static String apellidos;
-	
+    private static String carpetaDestino = "C:/MiAplicacion/imagenes/"; // Carpeta fija
 	private JTextField tfNombre;
 	private JTextField tfApellidos;
+	private JButton btnImagen;
+	private JLabel lblImagen;
+	
 
 	/**
 	 * Launch the application.
@@ -61,6 +72,8 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				crearCarpetaCentral();
 			}
 		});
 	}
@@ -84,9 +97,9 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 		JLabel lblGestionJugadores = new JLabel("Gestión de jugadores");
 		header.add(lblGestionJugadores);
 		
-		JPanel body = new JPanel();
+		body = new JPanel();
 		contentPane.add(body, BorderLayout.CENTER);
-		body.setLayout(new MigLayout("", "[275.00,grow][][43.00][grow]", "[][][grow]"));
+		body.setLayout(new MigLayout("", "[275.00,grow][0.00][126.00]", "[][][grow]"));
 		
 		JLabel lblNombre = new JLabel("Nombre: ");
 		body.add(lblNombre, "flowx,cell 0 0");
@@ -108,6 +121,10 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 		//********************************
 		// creo el modelo de datos de la lista
 		dlm = new DefaultListModel<Jugador>();
+		
+		btnImagen = new JButton("Añadir Imagen");
+		btnImagen.addActionListener(this);
+		body.add(btnImagen, "cell 2 1,alignx center");
 		// creo la lista
 		lstJugadores = new JList<>(dlm);
 		// asocio el modelo de datos a la lista
@@ -129,6 +146,12 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 		btnModificar = new JButton("Modificar");
 		btnModificar.addActionListener(this);
 		body.add(btnModificar, "cell 0 1,growx");
+		
+		lblImagen = new JLabel("[Imagen]");
+	 
+
+   	 	lblImagen.setHorizontalAlignment(SwingConstants.CENTER);
+   	 	body.add(lblImagen, "cell 2 2,alignx center");
 		
 		JPanel footer = new JPanel();
 		contentPane.add(footer, BorderLayout.SOUTH);
@@ -182,6 +205,9 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 	public void actionPerformed(ActionEvent ae) {
 		// TODO Auto-generated method stub
 		Object o = ae.getSource();
+		// obtenemos los elementos seleccionados de la lista
+		List<Jugador> ValSelec = lstJugadores.getSelectedValuesList();
+		int[] IndSelec = lstJugadores.getSelectedIndices();
 		//String nombre;
 		//String apellidos;
 		if(o == btnAñadir) {
@@ -224,8 +250,9 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 		}else if(o == btnEliminar) {
 			if (modificando == false) {
 				// obtenemos los elementos seleccionados de la lista
-				List<Jugador> ValSelec = lstJugadores.getSelectedValuesList();
-				int[] IndSelec = lstJugadores.getSelectedIndices();
+			
+				//List<Jugador> ValSelec = lstJugadores.getSelectedValuesList();
+				//int[] IndSelec = lstJugadores.getSelectedIndices();
 				
 				if (IndSelec.length <= 0) {
 					JOptionPane.showMessageDialog(this,(String)"No hay valores seleccionados ","error",JOptionPane.ERROR_MESSAGE);
@@ -242,12 +269,11 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 			
 			
 		}else if(o == btnModificar) {
-			
 			// empezamos indicando que no hay errores
 			error = false;
 			// obtenemos los valores seleccionados
-			List<Jugador> ValSelec = lstJugadores.getSelectedValuesList();
-			int[] IndSelec = lstJugadores.getSelectedIndices();
+			//List<Jugador> ValSelec = lstJugadores.getSelectedValuesList();
+			//int[] IndSelec = lstJugadores.getSelectedIndices();
 			Jugador j;
 			
 			// compruebo que solo haya un elemento seleccionado
@@ -260,13 +286,18 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 				
 				if (modificando == false) {
 					
-					
+					// pongo los datos del jugador visibles
 					tfNombre.setText(j.getNombre());
 					tfApellidos.setText(j.getApellidos());
+					lblImagen.setText("");
+					lblImagen.setIcon(fotoIcon = new ImageIcon(j.getRutaImagen()));
 					btnModificar.setText("Aplicar cambios");
 					
+					
+					
+					
 					modificando = true;
-				
+					lstJugadores.setEnabled(false);
 		
 			}else if (modificando == true) {
 				
@@ -274,7 +305,7 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 				for (int i = dlm.getSize() -1 ; i >= 0; i--) {
 					jug = dlm.getElementAt(i); // Obtenemos el jugador
 					if (jug.getNombre().equalsIgnoreCase(nombre) || jug.getApellidos().equalsIgnoreCase(apellidos)) {
-						JOptionPane.showMessageDialog(this, (String) "El jugador "+nombre+" "+apellidos+" ya esta en uso.", "Error",JOptionPane.ERROR_MESSAGE);
+						JOptionPane.showMessageDialog(this, (String) "El jugador "+nombre+" "+apellidos+" ya esta en el sistema", "Error",JOptionPane.ERROR_MESSAGE);
 						error = true;
 						break;
 					}
@@ -290,14 +321,98 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 					btnModificar.setText("Modificar");
 					GrabarJugadores("Jugadores.ser", dlm);
 				}
+				lstJugadores.setEnabled(true);
 				
 			}
 			}
 			
 			
-		}
+		}else if (o == btnImagen) {
 			
+			// compruebo que solo haya un elemento seleccionado
+			if (IndSelec.length <= 0 || IndSelec.length > 1) {
+				JOptionPane.showMessageDialog(this,(String)"debe selecionar un judador","error",JOptionPane.ERROR_MESSAGE);
+			}else {
+				/*
+				 * ABRO EXPLORADOR DE ARCHIVOS
+				 **/
+				// Crear un JFileChooser
+	            JFileChooser fileChooser = new JFileChooser();
+	            
+	            // PARA MODIFICAR LA CARPETA POR DEFECTO EN LA QUE SE ABRE EL EXPLORADOR DE ARCHIVOS
+	            //fileChooser.setCurrentDirectory(new File(""));
+	            
+	            // sera 1 cuando se pulsa cancelar o X y 0 cuando se pulsa abrir 
+	            int respuesta = fileChooser.showOpenDialog(null);
+				// obtengo el objeto seleccionado 
+    			Jugador j;
+				j = ValSelec.get(0);
+	            
+	            if (respuesta == JFileChooser.APPROVE_OPTION) {
+	            	/* OBTENGO DATOS DEL ARCHIVO SLEECCIONADO*/
+	            	// si pulsa abrir
+	            	// obtengo el archivo seleccionado
+	            	 File selectedFile = new File(fileChooser.getSelectedFile().getAbsolutePath());
+	            	 //obtengo el nombre del archivo
+	            	 String fileName = fileChooser.getSelectedFile().getName();
+	            	 // obtengo la ruta ORIGEN del archivo seleccionado
+	            	 String filePath = selectedFile.getAbsolutePath();
+	            	 
+	            	 
+	            	 /*OBTENGO DATOS DEL ARCHIVO DESTINO*/
+	            	 // creo la ruta DESTINO;
+	            	 File archivoDestino = new File(carpetaDestino + fileName);
+	            	 //obtengo el str de la ruta DESTINO
+	            	 String archivoDestinoStr = archivoDestino.getAbsolutePath();
+	            	 
+	            	 /*COMPRUEBO QUE NO HAYA UN JUGADOR CON ESA MISMA RUTA DE FOTO*/
+	            	 if(fotoRepe(archivoDestinoStr)) {
+	            		 JOptionPane.showMessageDialog(null, "No puede tener dos jugadores con la misma imagen.");
+	            	 }else{
+	            		 /*COPIO LA IMAGEN A LA CARPETA CENTRAL*/
+	            		 try {
+		                     // Copiar la imagen seleccionada a la carpeta central
+		                     Files.copy(selectedFile.toPath(), archivoDestino.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+		                     // Actualizar el JLabel con la imagen copiada
+		                     lblImagen.setIcon(new ImageIcon(archivoDestino.getAbsolutePath()));
+
+		                 } catch (IOException e) {
+		                     JOptionPane.showMessageDialog(null, "Error al copiar la imagen en "+carpetaDestino+".");
+		                     e.printStackTrace();
+		                 }
+		            	 
+		            	 
+		            	 // MUESTRO LA IMAGEN EN EL JFRAME
+		            	 fotoIcon = new ImageIcon(filePath);
+		            	 lblImagen.setText("");
+		            	 lblImagen.setIcon(fotoIcon);
+
+		            	 // APLICO LA NUEVA RUTA AL JUGADOR
+		            	 j.setRutaImagen(archivoDestinoStr);
+		            	 // aplicamos los cambios en el dlm
+		            	 dlm.set(IndSelec[0],j);
+		            	 // aplicamos los cambios en el .ser
+		            	 GrabarJugadores("Jugadores.ser", dlm);
+		            	 // para pruebas
+		            	 System.out.println(j.getNombre()+" "+filePath);
+	            		 
+	            	 };
+	            	 
+	            	 
+	            	 
+	           
+	            }
+				
+			}
+
+		}
+        
 	}
+	
+	
+			
+	
 	
 	
 	/*
@@ -337,9 +452,75 @@ public class VenatanaGestionJugadores extends JFrame implements ActionListener, 
 	        }
 	}
 	
-		
-	
+    public static void crearCarpetaCentral() {
+        //String carpetaDestino = "C:/MiAplicacion/imagenes/"; // Carpeta fija
+        File carpeta = new File(carpetaDestino);
+        if (!carpeta.exists()) {
+            if (carpeta.mkdirs()) {
+                System.out.println("Carpeta creada en: " + carpetaDestino);
+            } else {
+                System.out.println("No se pudo crear la carpeta.");
+            }
+        }
+    };
+
+    public static boolean fotoRepe(String rutaImagenNueva) {
+    	String rutaImagenVieja;
+    	for (int i = 0; i < dlm.size(); i++) {
+    		rutaImagenVieja = dlm.get(i).getRutaImagen();
+    		//System.out.println("comparando "+ rutaImagenNueva+" y "+rutaImagenVieja);
+    		if (rutaImagenVieja.equals(rutaImagenNueva)) {
+    			//System.out.println("hemos encontrado una conicidencia");
+    			return true;
+    		}	
+    	}
+    	
+    	return false;
+    		
+   }
 	
 	
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
