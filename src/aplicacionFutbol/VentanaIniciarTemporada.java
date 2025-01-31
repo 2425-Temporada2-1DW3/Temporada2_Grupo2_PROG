d86e595ab3be55dca3506233cf1356e8ed297e91
RobertTatillo <robrobertut@gmail.com>
2025-01-31 10:25:42 +0100

    Pdf hecho

--- src/aplicacionFutbol/VentanaIniciarTemporada.java
+++ src/aplicacionFutbol/VentanaIniciarTemporada.java
@@ -64,7 +64,7 @@ public class VentanaIniciarTemporada extends JFrame implements ActionListener, W
      * Create the frame.
      */
     public VentanaIniciarTemporada() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 450, 342);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
